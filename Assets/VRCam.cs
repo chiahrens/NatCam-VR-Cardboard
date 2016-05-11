927b613868e173e2eae61fcb38a29d78ee5ce3d0
Memuna Olokoba <maiisalihu@yahoo.com>
2016-05-11 12:17:24 -0400

    Add files via upload

---
+++ Assets/VRCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using NatCamU;
+
+public class VRCam : UnitygramBase {
+	
+	[Header("Demo")]
+	public Transform cube;
+		
+	// Use this for initialization
+	public override void Start () {
+		//Start Unitygram Base
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Spin the cube
+		cube.Rotate(0f, 2f, 0f);
+	}
+}
